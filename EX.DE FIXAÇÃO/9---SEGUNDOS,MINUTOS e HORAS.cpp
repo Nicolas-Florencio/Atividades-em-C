3ad083011e2b1fa530c684e59bf7e1a5c14e132f
unknown <nicolas.alves14@etec.sp.gov.br>
2019-09-29 12:27:05 -0300

    Nicolas 29/09/2019

--- EX.DE FIXAÇÃO/9---SEGUNDOS,MINUTOS e HORAS.cpp
+++ EX.DE FIXAÇÃO/9---SEGUNDOS,MINUTOS e HORAS.cpp
@@ -8,5 +8,18 @@
 #include <locale>
 int main(){
 	setlocale(LC_ALL, "PORTUGUESE");
-	
+		int tempo=0 ,hora=0, minuto=0, segundo=0;
+		printf("Informe os segundos: ");
+		scanf("%i", & tempo);
+		hora= tempo/3600;
+		minuto=tempo/60;
+		if (tempo==60){
+			minuto=1;
+			hora=0;
+			printf("%i segundos em:\n\nHoras: %i\n\nMinutos: %i\n\nSegunos: %i\n\n",tempo,hora,minuto,tempo);
+		}
+		else{
+			printf("%i segundos em:\n\nHoras: %i\n\nMinutos: %i\n\nSegunos: %i\n\n",tempo,hora,minuto,tempo);
+		}
+		system("Pause");
 }
